26f0d767379046e32f5b87593ef948aa83b31eb8
Saul <zhuhao9966@gmail.com>
2025-04-28 01:34:56 +0800

    chore: remove legacy test_backtest.py

--- test_backtest.py
+++
@@ -1,8 +0,0 @@
-# test_backtest.py
-from backtest_ma import equity
-
-def test_equity_positive():
-    """
-    Smoke test: ensure the backtest doesn't lose all capital.
-    """
-    assert equity > 0  # toy check: you didn't lose everything! 
